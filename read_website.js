4d1f92eb66035af57d318193db7672fa21d023a5
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-08 18:55:57 +0800

    Update read_website.js

--- read_website.js
+++ read_website.js
@@ -2,6 +2,21 @@ const puppeteer = require('puppeteer');
 const moment = require('moment');
 const fs = require('fs');
 
+async function crawlUrl(url, browser) {
+  try {
+    const page = await browser.newPage();
+    await page.goto(url);
+    const content = await page.content();
+    const timestamp = moment().format('YYYYMMDDHHmmss');
+    const filename = `${url.replace(/[:/]/g, '_')}_${timestamp}.txt`;
+    fs.writeFileSync(filename, content);
+    console.log(`Successfully crawled ${url}`);
+    await page.close();
+  } catch (error) {
+    console.log(`Failed to crawl ${url}: ${error.message}`);
+  }
+}
+
 async function main() {
   const filepath = process.argv[2];
   const browser = await puppeteer.launch({ headless: "new" });
@@ -9,28 +24,24 @@ async function main() {
   try {
     const urls = fs.readFileSync(filepath, 'utf8').split('\n').filter(Boolean);
 
-    for (let i = 0; i < urls.length; i++) {
-      const url = urls[i];
-      try {
-        const page = await browser.newPage();
-        await page.goto(url);
-        const content = await page.content();
-        const timestamp = moment().format('YYYYMMDDHHmmss');
-        const filename = `${url.replace(/[:/]/g, '_')}_${timestamp}.txt`;
-        fs.writeFileSync(filename, content);
-        console.log(`Successfully crawled ${url}`);
-      } catch (error) {
-        console.log(`Failed to crawl ${url}: ${error.message}`);
-        continue; // Skip to the next iteration
-      } finally {
-        if (page) {
-          await page.close();
-        }
+    async function processUrls(index) {
+      if (index >= urls.length) {
+        // Finished processing all urls
+        await browser.close();
+        return;
       }
+
+      const url = urls[index];
+      await crawlUrl(url, browser);
+
+      // Process next url
+      await processUrls(index + 1);
     }
+
+    // Start processing urls
+    await processUrls(0);
   } catch (error) {
     console.log(`Failed to read file: ${error.message}`);
-  } finally {
     await browser.close();
   }
 }
