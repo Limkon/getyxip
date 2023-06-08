b413e3fd5ee1b5da1343e35a9f595f6dd142637b
Limkon <130622161+Limkon@users.noreply.github.com>
2023-06-08 19:26:11 +0800

    Update read_website.js

--- read_website.js
+++ read_website.js
@@ -1,16 +1,6 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
 const fs = require('fs');
-const fetch = require('node-fetch');
-
-async function isUrlAccessible(url) {
-  try {
-    const response = await fetch(url);
-    return response.ok;
-  } catch (error) {
-    return false;
-  }
-}
 
 async function main() {
   const filepath = process.argv[2];
@@ -21,23 +11,18 @@ async function main() {
 
     for (let i = 0; i < urls.length; i++) {
       const url = urls[i];
-      const isAccessible = await isUrlAccessible(url);
 
-      if (isAccessible) {
-        try {
-          const page = await browser.newPage();
-          await page.goto(url);
-          const content = await page.content();
-          const timestamp = moment().format('YYYYMMDDHHmmss');
-          const filename = `${url.replace(/[:/]/g, '_')}_${timestamp}.txt`;
-          fs.writeFileSync(filename, content);
-          console.log(`Successfully crawled ${url}`);
-          await page.close();
-        } catch (error) {
-          console.log(`Failed to crawl ${url}: ${error.message}`);
-        }
-      } else {
-        console.log(`URL not accessible: ${url}`);
+      try {
+        const page = await browser.newPage();
+        await page.goto(url);
+        const content = await page.content();
+        const timestamp = moment().format('YYYYMMDDHHmmss');
+        const filename = `${url.replace(/[:/]/g, '_')}_${timestamp}.txt`;
+        fs.writeFileSync(filename, content);
+        console.log(`Successfully crawled ${url}`);
+        await page.close();
+      } catch (error) {
+        console.log(`Failed to crawl ${url}: ${error.message}`);
       }
     }
   } catch (error) {
